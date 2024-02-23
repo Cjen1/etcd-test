2363e17c885d83ba58438cb0786eba6d6524e24e
cjen1 <cjj39@cam.ac.uk>
2024-02-23 15:03:42 +0000

    update cluster

--- cluster.sh
+++ cluster.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-H1=ms0924.utah.cloudlab.us
-H2=c220g5-111228.wisc.cloudlab.us
-H3=clnode310.clemson.cloudlab.us
-H4=apt121.apt.emulab.net
+H1=ms0835.utah.cloudlab.us
+H2=c220g2-011026.wisc.cloudlab.us
+H3=clnode227.clemson.cloudlab.us
+H4=ms0819.utah.cloudlab.us
 >&2 echo $H1
 >&2 echo $H2
 >&2 echo $H3
--- record_perf.sh
+++ record_perf.sh
@@ -17,9 +17,9 @@ for server in H1 H2 H3 H4; do
     ssh Cjen1@${!server} "iperf3 -s -1" > /dev/null &
     SERVER_PID=$!
     sleep 0.5
-    BANDWIDTH=$(ssh Cjen1@${!client} "iperf3 -c ${!server} -J" | jq '.end.streams[].receiver.bits_per_second' | numfmt --to=si)
-
-    LATENCY=$(ssh Cjen1@${!client} "ping -c 1 ${!server}" | sed -ne '/.*time=/{;s///;s/\..*//;p;}')
+    OUT=$(ssh Cjen1@${!client} "iperf3 -c ${!server} -J")
+    BANDWIDTH=$(echo $OUT | jq '.end.streams[].receiver.bits_per_second' | numfmt --to=si)
+    LATENCY=$(echo $OUT | jq '.end.streams[].sender.mean_rtt')
 
     echo "$client, $server, $BANDWIDTH, $LATENCY"
   done
